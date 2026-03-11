a2b9538e464699cf7ec148f16f2438167b8ffdcf
Ramesh Chandra Majhee <40921575+rameshmajhee@users.noreply.github.com>
2026-03-11 21:03:49 +0530

    day-4-state

---
+++ day-4-terraform-state-file/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "demo-vpc"
+    }  
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.medium"
+    tags = {
+        Name = "test-instance"
+    }   
+  
+}
